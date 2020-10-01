6a771066974c72376c9582b3b591771c09ca68d2
Chris-Windows-Desktop <cmendez20@utexas.edu>
2020-09-30 22:45:26 -0500

    Built navbar

---
+++ index.js
@@ -0,0 +1,11 @@
+// mobile menu
+const burgerIcon = document.querySelector('#burger');
+const navbarMenu = document.querySelector('#nav-links');
+
+burgerIcon.addEventListener('click', () => {
+  navbarMenu.classList.toggle('is-active');
+});
+
+navbarMenu.addEventListener('click', () => {
+  navbarMenu.classList.toggle('is-active');
+});
